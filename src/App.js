13e2c56ad9e63720fcd4153d5f6d0c9f59c22869
Sam Meyer <sam.meyer@procore.com>
2020-04-03 18:55:33 -0700

    first attempt

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import React from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import confetti from 'canvas-confetti';
 
 function App() {
+  const [seconds, setSeconds] = useState(10);
+  const [ready, setReady] = useState(false);
+
+  function time0(interval) {
+    clearInterval(interval)
+    confetti()
+  };
+
+  const startCountdown = useCallback(() => {
+    let interval = null;
+      interval = setInterval(() => {
+        setSeconds(seconds => seconds - 1);
+      }, 1000);
+     if (seconds === 0) {
+      time0(interval);
+    }
+    return () => clearInterval(interval);
+  }, [seconds]);
+  console.log('ready?', ready)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="item">
+        <h1>{ ready ? seconds : 'Ready?' }</h1>
+        { !ready && (
+
+<div class="buttons">
+  <button class="blob-btn">
+    Let's Go!
+    <span class="blob-btn__inner">
+      <span class="blob-btn__blobs">
+        <span class="blob-btn__blob"></span>
+        <span class="blob-btn__blob"></span>
+        <span class="blob-btn__blob"></span>
+        <span class="blob-btn__blob"></span>
+      </span>
+    </span>
+  </button>
+  <br/>
+
+<svg xmlns="http://www.w3.org/2000/svg" version="1.1">
+  <defs>
+    <filter id="goo">
+      <feGaussianBlur in="SourceGraphic" result="blur" stdDeviation="10"></feGaussianBlur>
+      <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0 0 1 0 0 0 0 0 1 0 0 0 0 0 21 -7" result="goo"></feColorMatrix>
+      <feBlend in2="goo" in="SourceGraphic" result="mix"></feBlend>
+    </filter>
+  </defs>
+</svg>)}
+
+      </div>
     </div>
   );
 }
