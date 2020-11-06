f6f4da62da49ee1d0ea078a7b232d264edda9ff5
Sam Meyer <sam.meyer@procore.com>
2020-11-06 10:30:15 -0800

    feat: First pass at adding sound

--- src/App.js
+++ src/App.js
@@ -1,58 +1,90 @@
-import React, { useState, useCallback, useEffect } from 'react';
-import logo from './logo.svg';
+import React, { useState, useRef, useCallback,  } from 'react';
 import './App.css';
 import confetti from 'canvas-confetti';
+import useSound from 'use-sound';
+import { crowd, fireworks, pop, horn, ticktock } from './sounds';
 
+import { useEffect } from 'react';
+
+function Party({ isParty }) {
+  const [playHorn] = useSound(horn, { volume: 0.05 });
+  const [playCrowd] = useSound(crowd, { volume: 0.25 });
+  const [playFireworks] = useSound(fireworks, { volume: 0.25 });
+
+  useEffect(() => {
+    if (isParty) {
+      playCrowd();
+      playHorn();
+      playFireworks();
+    }
+  }, [isParty, playCrowd, playFireworks, playHorn]);
+
+  return (isParty && <h1>yay!</h1>);
+}
 function App() {
-  const [seconds, setSeconds] = useState(10);
+  const [seconds, setSeconds] = useState(5);
   const [ready, setReady] = useState(false);
+  const [playTick] = useSound(ticktock, { volume: 0.25 });
+  const [playPop] = useSound(pop, { volume: 0.25});
 
-  function time0(interval) {
-    clearInterval(interval)
-    confetti()
-  };
+  function celebrate() {
+    var duration = 15 * 1000;
+    var animationEnd = Date.now() + duration;
+    var defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
+
+    function randomInRange(min, max) {
+      return Math.random() * (max - min) + min;
+    }
+
+    var interval = setInterval(function() {
+      var timeLeft = animationEnd - Date.now();
+
+      if (timeLeft <= 0) {
+        return clearInterval(interval);
+      }
+
+      var particleCount = 50 * (timeLeft / duration);
+      // since particles fall down, start a bit higher than random
+      confetti(Object.assign({}, defaults, { particleCount, origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 } }));
+      confetti(Object.assign({}, defaults, { particleCount, origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 } }));
+    }, 250);
+
+  }
+
+  const intervalRef = useRef();
 
   const startCountdown = useCallback(() => {
-    let interval = null;
-      interval = setInterval(() => {
-        setSeconds(seconds => seconds - 1);
-      }, 1000);
-     if (seconds === 0) {
-      time0(interval);
-    }
-    return () => clearInterval(interval);
-  }, [seconds]);
-  console.log('ready?', ready)
+    playPop()
+    confetti({
+      particleCount: 50,
+      spread: 30,
+      startVelocity: 20,
+      origin: { y: 0.6 }
+    });
+
+    intervalRef.current = setInterval(() => {
+      setSeconds(seconds => seconds - 1);
+      playTick();
+    }, 1000);
+
+    return () => clearInterval(intervalRef.current);
+  }, [playPop, playTick]);
+
+  const timesUp = seconds === 0;
+
+  if (timesUp) {
+    clearInterval(intervalRef.current);
+    celebrate()
+  }
+
+  const isParty = timesUp && ready;
+
   return (
     <div className="app">
       <div className="item">
-        <h1>{ ready ? seconds : 'Ready?' }</h1>
-        { !ready && (
-
-<div class="buttons">
-  <button class="blob-btn">
-    Let's Go!
-    <span class="blob-btn__inner">
-      <span class="blob-btn__blobs">
-        <span class="blob-btn__blob"></span>
-        <span class="blob-btn__blob"></span>
-        <span class="blob-btn__blob"></span>
-        <span class="blob-btn__blob"></span>
-      </span>
-    </span>
-  </button>
-  <br/>
-
-<svg xmlns="http://www.w3.org/2000/svg" version="1.1">
-  <defs>
-    <filter id="goo">
-      <feGaussianBlur in="SourceGraphic" result="blur" stdDeviation="10"></feGaussianBlur>
-      <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0 0 1 0 0 0 0 0 1 0 0 0 0 0 21 -7" result="goo"></feColorMatrix>
-      <feBlend in2="goo" in="SourceGraphic" result="mix"></feBlend>
-    </filter>
-  </defs>
-</svg>)}
-
+        {!isParty && <h1 className={ready && !timesUp ? 'counting' : ''}>{ ready ? seconds : 'Ready?' }</h1>}
+        <Party isParty={isParty} />
+        { !ready && <button className='go-button' onClick={() => {  startCountdown() && setReady(true)}}>Start!</button>}
       </div>
     </div>
   );
---
+++ src/sounds/index.js
@@ -0,0 +1,13 @@
+import fireworks from './fireworks.mp3';
+import horn from './horn.mp3';
+import pop from './pop.mp3';
+import ticktock from './ticktock.mp3';
+import crowd from './crowd.mp3'
+
+export {
+  crowd,
+  horn,
+  fireworks,
+  pop,
+  ticktock,
+};
